2d6210a9d61818ec602618c94a6c0b368b75af21
emmanuel-toast <emmanuel@toast.co.nz>
2023-08-08 14:21:52 +1200

    adding a check to make sure the  is passed to the function before looking for it with the helper

--- src/Extensions/PageExtension.php
+++ src/Extensions/PageExtension.php
@@ -15,12 +15,12 @@ class PageExtension extends DataExtension
 {
     public function getThemeColour($colour)
     {
-        return Helper::getThemeColourFromColourPaletteID($colour);
+        return ($colour) ? Helper::getThemeColourFromColourPaletteID($colour) : null;
     }
 
     public function getThemeColourFromID($colour)
     {
-        return Helper::getThemeColourFromID($colour);
+        return ($colour) ? Helper::getThemeColourFromID($colour) : null;
     }
 }
 
